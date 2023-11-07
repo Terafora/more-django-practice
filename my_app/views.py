69aa664c12169d3239cb26cd56068f3f8bb74316
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-07 15:18:13 +0000

    added a dynamic hello page

--- my_app/views.py
+++ my_app/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def about(request):
     return HttpResponse("My name is Charlotte. I'm a multi-lingual full-stack web developer. Autumn is my favourite season and I hope to go back to swimming most days of the week after I finally have my ops.")
+
+def hello(request, first_name):
+    return HttpResponse(f"Hello {first_name}")
