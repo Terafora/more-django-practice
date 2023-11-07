da1756a02f885f3b26aa5283f2931e4392c65795
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-07 15:08:05 +0000

    Completed a challenge on the tutorials I was following

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+def about(request):
+    return HttpResponse("My name is Charlotte. I'm a multi-lingual full-stack web developer. Autumn is my favourite season and I hope to go back to swimming most days of the week after I finally have my ops.")
